9c355d447d49585d69192f878c8e51287235e6b5
ketjehdev <ketjeh.developer@gmail.com>
2022-09-05 13:27:53 +0700

    update navbar bottom

--- resources/views/layout/navBottom.blade.php
+++ resources/views/layout/navBottom.blade.php
@@ -1,7 +1,7 @@
 <div class="bot-nav">
-    <div class="d-flex flex-column text-danger align-items-center" title="Home">
+    <div class="d-flex flex-column align-items-center" title="Home">
         <a href="">
-            <i class="fa fa-home text-danger" aria-hidden="true"></i>
+            <i class="fa fa-home @if($page == 'home') text-success @endif" aria-hidden="true"></i>
         </a>
         <span style="font-size: 10px;">Home</span>
     </div>
@@ -10,21 +10,21 @@
         <a href="">
             <i class="fa fa-laptop text-secondary" aria-hidden="true"></i>
         </a>
-        <span style="font-size: 10px;">Portfolio</span>
+        <span style="font-size: 10px;">Trend Bisnis</span>
     </div>
 
     <div class="d-flex text-secondary flex-column align-items-center" title="Services">
         <a href="">
             <i class="fa fa-cloud text-secondary" aria-hidden="true"></i>
         </a>
-        <span style="font-size: 10px;">Services</span>
+        <span style="font-size: 10px;">Support Bisnis</span>
     </div>
 
     <div class="d-flex text-secondary flex-column align-items-center" title="About Me">
         <a href="">
-            <i class="fa fa-user text-secondary" aria-hidden="true"></i>
+            <i class="fa fa-users text-secondary" aria-hidden="true"></i>
         </a>
-        <span style="font-size: 10px;">About Me</span>
+        <span style="font-size: 10px;">About Us</span>
     </div>
 
 </div>
